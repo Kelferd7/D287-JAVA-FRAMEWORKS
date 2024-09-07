42226839bc3f808cdbe7706490f2b4a0c09a7a84
Kelferd7 <kbuhlig7@gmail.com>
2024-09-07 13:02:26 -0500

    Updated BootStrapData.java for 5 "parts" and 5 "products" with if-statement condition to prevent duplicates: lines 43-50/52-85/93-137. Date: 9/7.

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -1,8 +1,10 @@
 package com.example.demo.bootstrap;
 
+import com.example.demo.domain.InhousePart;
 import com.example.demo.domain.OutsourcedPart;
 import com.example.demo.domain.Part;
 import com.example.demo.domain.Product;
+import com.example.demo.repositories.InhousePartRepository;
 import com.example.demo.repositories.OutsourcedPartRepository;
 import com.example.demo.repositories.PartRepository;
 import com.example.demo.repositories.ProductRepository;
@@ -38,34 +40,101 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        if(partRepository.count() == 0) {
 
-       /*
-        OutsourcedPart o= new OutsourcedPart();
-        o.setCompanyName("Western Governors University");
-        o.setName("out test");
-        o.setInv(5);
-        o.setPrice(20.0);
-        o.setId(100L);
-        outsourcedPartRepository.save(o);
-        OutsourcedPart thePart=null;
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
-        for(OutsourcedPart part:outsourcedParts){
-            if(part.getName().equals("out test"))thePart=part;
+            //Set single items
+            Part beefJerky = new InhousePart();
+            beefJerky.setName("Beef Jerky");
+            beefJerky.setInv(10);
+            beefJerky.setPrice(15);
+            beefJerky.setId(5);
+
+            //Save to part repository
+            partRepository.save(beefJerky);
+
+            Part oreos = new InhousePart();
+            oreos.setName("Oreos");
+            oreos.setInv(12);
+            oreos.setPrice(2);
+            oreos.setId(6);
+
+            partRepository.save(oreos);
+
+            Part nutterButters = new InhousePart();
+            nutterButters.setName("Nutter Butters");
+            nutterButters.setInv(15);
+            nutterButters.setPrice(3);
+            nutterButters.setId(7);
+
+            partRepository.save(nutterButters);
+
+            Part riceKrispies = new InhousePart();
+            riceKrispies.setName("Peanut Butter Rice Krispies");
+            riceKrispies.setInv(20);
+            riceKrispies.setPrice(4);
+            riceKrispies.setId(8);
+
+            partRepository.save(riceKrispies);
+
+            Part puppyChow = new InhousePart();
+            puppyChow.setName("Puppy Chow");
+            puppyChow.setInv(10);
+            puppyChow.setPrice(6);
+            puppyChow.setId(9);
+
+            partRepository.save(puppyChow);
         }
 
-        System.out.println(thePart.getCompanyName());
-        */
         List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
         for(OutsourcedPart part:outsourcedParts){
             System.out.println(part.getName()+" "+part.getCompanyName());
         }
 
-        /*
-        Product bicycle= new Product("bicycle",100.0,15);
-        Product unicycle= new Product("unicycle",100.0,15);
-        productRepository.save(bicycle);
-        productRepository.save(unicycle);
-        */
+        if(productRepository.count() == 0) {
+
+            //Set bundle items
+            Product jerkyBundle = new Product();
+            jerkyBundle.setName("Jerky Bundle");
+            jerkyBundle.setInv(12);
+            jerkyBundle.setPrice(25);
+            jerkyBundle.setId(0);
+
+            //Save to product repository
+            productRepository.save(jerkyBundle);
+
+            Product cookieBundle = new Product();
+            cookieBundle.setName("Cookie Bundle");
+            cookieBundle.setInv(12);
+            cookieBundle.setPrice(15);
+            cookieBundle.setId(1);
+
+            productRepository.save(cookieBundle);
+
+            Product sourCandy = new Product();
+            sourCandy.setName("Sour Candy Bundle");
+            sourCandy.setInv(15);
+            sourCandy.setPrice(10);
+            sourCandy.setId(2);
+
+            productRepository.save(sourCandy);
+
+            Product everythingBundle = new Product();
+            everythingBundle.setName("Little Bit of Everything Bundle");
+            everythingBundle.setInv(15);
+            everythingBundle.setPrice(20);
+            everythingBundle.setId(3);
+
+            productRepository.save(everythingBundle);
+
+            Product chipBundle = new Product();
+            chipBundle.setName("Wackadoo Chips Bundle");
+            chipBundle.setInv(15);
+            chipBundle.setPrice(25);
+            chipBundle.setId(4);
+
+            productRepository.save(chipBundle);
+
+        }
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Products"+productRepository.count());
